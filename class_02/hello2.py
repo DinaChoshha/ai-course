dbae7cd72a80c55505e67de1f835352c0e6f6178
DinaChoshha <dinachoshha@gmail.com>
2025-10-23 21:03:23 +0300

    second lesson exercises

---
+++ class_02/hello2.py
@@ -0,0 +1,47 @@
+# %%
+
+x = ["a", "b"]
+y = ["a", "b"]
+
+if x == y:
+    print("True")
+else:
+    print("False")
+
+# %%
+
+tuple = ("stam",)
+
+print(type(tuple))
+
+tuple1 = ("stam")
+
+print(type(tuple1))
+# %%
+print(list("python"))
+# %%
+tuple_1 = (1, 2, 3)
+tuple_2 = ("yes","sure")
+
+tuple_11 = tuple_1 + tuple_2
+tuple_22 = tuple_1, tuple_2
+
+print(len(tuple_11))
+print(len(tuple_22))
+# %%
+x = 0
+
+if x != 0:
+    print("Nope 1!")
+
+if x != None:
+    print("Nope 2!")
+# %%
+x = ""
+
+if x != 0:
+    print("Nope 1!")
+
+if x != None:
+    print("Nope 2!")
+# %%
